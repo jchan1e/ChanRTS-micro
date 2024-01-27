89792f101caa006e55fc25e40729439b503ea988
jordan <jchan1e@gmail.com>
2024-01-27 13:51:49 -0700

    implemented generic agent class that can use any ML model internally

---
+++ agent.py
@@ -0,0 +1,116 @@
+#
+# generic agent class which can be trained and can play in real time
+
+import numpy as np
+import gym_microrts
+import torch
+import torch.nn as nn
+import torch.optim as optim
+#from torch.distribution.categorical import Categorical
+
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def softmax(x, axis=None):
+    x = x-x.max(axis=axis, keepdims=True)
+    y = np.exp(x)
+    return y/y.sum(axis=axis, keepdims=True)
+
+def sample(x):
+    p = softmax(x, axis=1)
+    #print(p.shape)
+    c = p.cumsum(axis=1)
+    u = np.random.rand(len(c), 1)
+    choices = (u < c).argmax(axis=1)
+    #print(choices.shape)
+    return choices.reshape(-1,1)
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+
+class Model(nn.Module):
+    #mapsize = None
+    def __init__(self, obs_shape, action_shape):
+        super().__init__()
+        mapsize = (obs_shape[0], obs_shape[1])
+        self.network = nn.Sequential(
+                layer_init(nn.Conv2d(obs_shape[2], 20, kernel_size=3, padding=1)),
+                nn.ReLU(),
+                layer_init(nn.Conv2d(20, 32, kernel_size=3, padding=1)),
+                nn.ReLU(),
+                nn.Flatten(0,-1),
+                layer_init(nn.Linear(32*mapsize[0]*mapsize[1], 256)),
+                #layer_init(nn.Linear(32*16*16, 256)),
+                nn.ReLU(),
+                )
+        self.actor = layer_init(nn.Linear(256, np.prod(action_shape)), std=0.01)
+
+    def forward(self, x):
+        #print(x.shape)
+        y = self.network(x.permute((2,0,1)))
+        #print(y.shape)
+        return self.actor(y)
+
+class Agent():
+    # class variables
+    env = None
+    obs = None
+    action = None
+    model = None
+    mapsize = None
+
+    def __init__(self, agent_type, env):
+        # assign class variables
+        self.env = env
+        # populate initial observation
+
+        # build model
+        #if agent_type == "simple":
+        #    pass
+        obs_shape = env.observation_space.shape
+        action_shape = env.get_action_mask().shape
+        #print(obs_shape)
+        #print(action_shape)
+        self.model = Model(obs_shape, action_shape)
+        #self.model = Model(obs_shape, action_shape).to(device)
+
+    @classmethod
+    def fromFile(cls, modelfile):
+        f = open(filename, "r")
+        self.model = torch.load(modelfile)
+        f.close()
+
+    def save(self, filename):
+        f = open(filename, "w")
+        torch.save(self.model, f)
+        f.close()
+
+    def set_obs(self, obs):
+        self.obs = torch.from_numpy(obs.astype(np.float32))
+
+    def set_action_mask(self, action_mask):
+        self.action_mask = torch.from_numpy(action_mask)
+
+    def get_action(self):
+        y = self.model.forward(self.obs)
+        #print(self.action_mask.shape)
+        #print(y.shape)
+        y = y.reshape(self.action_mask.shape)
+        y = self.action_mask * y
+        y = y.detach().numpy() # This is where it should return to main RAM and run on cpu
+        self.action = np.concatenate(
+            (
+                sample(y[:, 0:6]),
+                sample(y[:, 6:10]),
+                sample(y[:, 10:14]),
+                sample(y[:, 14:18]),
+                sample(y[:, 18:22]),
+                sample(y[:, 22:29]),
+                sample(y[:, 29:78]),
+            ),
+            axis=1,
+        )
+        #print(self.action.shape)
+        return self.action
--- game.py
+++ game.py
@@ -1,41 +1,47 @@
 #
 # Using Gym, start a Game and attach an Agent to play it
 
+import sys
 import numpy as np
 import gym
 import gym_microrts
 from gym_microrts import microrts_ai    # built-in AI agents
 from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv    # The Gym Env that runs the game
+from agent import Agent
+
+import time
+
+np.set_printoptions(threshold=sys.maxsize)
 
 
-class Agent:
-    def get_action(self, env, obs):
-        return env.action_sample.sample()
-
-def softmax(x, axis=None):
-    x = x - x.max(axis=axis, keepdims=True)
-    y = np.exp(x)
-    return y / y.sum(axis=axis, keepdims=True)
-
-def sample(logits):
-    # https://stackoverflow.com/a/40475357/6611317
-    # Takes an array of weighted probabilities, normalizes using softmax,
-    #   selects one by its probability, then returns the array in 1-hot form
-    p = softmax(logits, axis=1)
-    c = p.cumsum(axis=1)
-    u = np.random.rand(len(c), 1)
-    choices = (u < c).argmax(axis=1)
-    return choices.reshape(-1, 1)
+#def softmax(x, axis=None):
+#    x = x - x.max(axis=axis, keepdims=True)
+#    y = np.exp(x)
+#    return y / y.sum(axis=axis, keepdims=True)
+#
+#def sample(logits):
+#    # https://stackoverflow.com/a/40475357/6611317
+#    # Takes an array of weighted probabilities, normalizes using softmax,
+#    #   selects one by its probability, then returns the array of indeces chosen
+#    p = softmax(logits, axis=1)
+#    c = p.cumsum(axis=1)
+#    u = np.random.rand(len(c), 1)
+#    choices = (u < c).argmax(axis=1)
+#    return choices.reshape(-1, 1)
 
 
 if __name__ == "__main__":
-    agent1 = microrts_ai.coacAI
-    agent2 = microrts_ai.mixedBot
+    bot_agent = microrts_ai.coacAI
     env = MicroRTSGridModeVecEnv(
-            num_selfplay_envs=2,
+            num_selfplay_envs=0,
             num_bot_envs=1,
-            ai2s=[agent1]
+            ai2s=[bot_agent],
+            #map_paths=["maps/4x4/base4x4.xml"]
+            #map_paths=["maps/10x10/basesWorkers10x10.xml"]
+            map_paths=["maps/16x16/basesWorkers16x16.xml"]
     )
+    obs = env.reset()
+    agent1 = Agent("", env)
 
     # Observation space:
     #   Feature     #_planes    Values
@@ -54,33 +60,49 @@ if __name__ == "__main__":
     #   Relative Attack Position    [0:a^2-1] Where a=7 (max attack range 3)
 
     done = np.array([False,False,False])
-    obs = env.reset()[0]
     nvec = env.action_space.nvec
     while not done.any():
         env.render()
 
+        #time.sleep(1.0/30)
+
         #action = agent1.get_action(env, obs)
         action_mask = env.get_action_mask()
         action_mask = action_mask.reshape(-1, action_mask.shape[-1])
+        #print(action_mask.shape)
         # Set invalid actions to strong negative value
-        action_mask[action_mask == 0] = -9e8
+        #action_mask[action_mask == 0] = -9e8
 
         # Combine action mask with Agent Output
+        #action_mask[action_mask == 1] = agent1.get_action(obs)
 
-        # sample valid actions
-        action = np.concatenate(
-            (
-                sample(action_mask[:, 0:6]),    # action type:                  [None, Move, Harvest, Return, Build, Attack]
-                sample(action_mask[:, 6:10]),   # move parameter:               [N, E, S, W]
-                sample(action_mask[:, 10:14]),  # harvest parameter:            [N, E, S, W]
-                sample(action_mask[:, 14:18]),  # return parameter:             [N, E, S, W]
-                sample(action_mask[:, 18:22]),  # produce_direction parameter:  [N, E, S, W]
-                sample(action_mask[:, 22:29]),  # produce_unit_type parameter:  [Resource, Base, Barrack, Worker, Light, Heavy, Ranged]
-                # attack_target parameter
-                sample(action_mask[:, 29 : sum(env.action_space.nvec[1:])]),
-            ),
-            axis=1,
-        )
+        # randomly sample valid action parameters
+        #action = np.concatenate(
+        #    (
+        #        sample(action_mask[:, 0:6]),    # action type:                  [None, Move, Harvest, Return, Build, Attack]
+        #        sample(action_mask[:, 6:10]),   # move parameter:               [N, E, S, W]
+        #        sample(action_mask[:, 10:14]),  # harvest parameter:            [N, E, S, W]
+        #        sample(action_mask[:, 14:18]),  # return parameter:             [N, E, S, W]
+        #        sample(action_mask[:, 18:22]),  # produce_direction parameter:  [N, E, S, W]
+        #        sample(action_mask[:, 22:29]),  # produce_unit_type parameter:  [Resource, Base, Barrack, Worker, Light, Heavy, Ranged]
+        #        # attack_target parameter
+        #        sample(action_mask[:, 29 : sum(env.action_space.nvec[1:])]),
+        #    ),
+        #    axis=1,
+        #)
+        #print(obs)
+        #print(action)
+        #print()
+
+        # inform the agent of the game state
+        agent1.set_obs(obs[0])
+        agent1.set_action_mask(action_mask)
+
+        # Get the agent's action vector
+        action = agent1.get_action()
+
+        # Grid mode, concatenate all actions in an array
         obs, reward, done, info = env.step(action)
+        #print(obs.shape)
 
     env.close()
